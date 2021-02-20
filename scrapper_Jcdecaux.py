51ba62a4203775309dad6b070935076b092a4625
Eoghan-dev <eoghan.cullen1@ucdconnect.ie>
2021-02-20 18:16:02 +0000

    big update

--- scrapper_Jcdecaux.py
+++ scrapper_Jcdecaux.py
@@ -1,30 +1,56 @@
-#myPrivates should be a python file containing you key for jcdecaux
-from myPrivates import myKey
+'''
+Import all modules needed to request the data and convert to the 
+table format needed to store on rds database.
 
+myPrivates.py should contain sensitive information 
+required to access the API and rds database.
+'''
+import myPrivates
+import requests
+import json
+import traceback
+import datetime
+import salalchemy.dialects
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, DateTime
+
+'''
+Below variables used in f strings are declared. These
+variables specifically the priavte ones are defined here
+for ease of reading while constructing strings
+'''
 name = "dublin"
-stations = "https://api.jcdecaux.com/vls/v1/stations"
-apikey = myKey
+stations_API = "https://api.jcdecaux.com/vls/v1/stations"
+apikey = myPrivates.myKey
+dbPass = myPrivates.dbPass
+db_url = myPrivates.dbURL
+user = "admin"
+dbName = "dbikes"
+sqlport = "3306"
 
+'''
+Create sql engine here as to not recreate every time data
+is sent to the rds database
+'''
+engine = create_engine(f"mysql+mysqlconnector://{user}:{dbPass}@{db_url}:{sqlport}/{dbName}")
 
 #run all the time
-#some changes need to be made for sql connection
+while True:
+	try:
+		r = requests.get(stations,params={"apiKey":apikey, "contract":name})
+		values = list(map(get_station,r.json()))
+		send2mysql(engine,values)
 
-def main():
-	while True:
-		try:
-			r = requests.get(stations,params={"apiKey":apikey, "contract":name})
-			store(json.loads(r.text))
-
-			#sleep for 5 minutes
-		except:
-			print(traceback.format_exc())
+		#sleep for 5 minutes
+	except:
+		print(traceback.format_exc())
 
 
 
-	def w2panda_db():
-		#write json request to panda db
-		pass
+def get_station(obj):
+	#make list of dicts from json
+	pass
 
-	def send2aws():
-		#send data to aws database
-		pass
+def send2mysql():
+	#send to rds database
+	pass
