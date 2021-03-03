6d70b6bc08bfdfc5914e792a3275bbd83bf52092
Eoghan-dev <eoghan.cullen1@ucdconnect.ie>
2021-03-03 17:43:07 +0000

    Version 1 of the flask web app. Need to get google api key before testing can resume

---
+++ static/app.js
@@ -0,0 +1,36 @@
+let map;
+
+function initMap(){
+	fetch("/stations").then(response => {
+		return response.json()
+	}).then(data => {
+		console.log("data: ", data);
+
+		map = new google.maps.Map(document.getElementById("map"),{
+				center: {lat: 53.34632331338235, lng: -6.26150959615664},
+				zoom: 13,
+				//read documentation and find features
+		});
+
+		data.forEach(station => {
+			marker = new google.maps.Marker({
+				position: {lat: station.pos_lat, lng: station.pos_long},
+				map: map,
+			});
+			marker.addListener("click", () => {
+				const infowindow = new google.maps.InfoWindow({
+					content: station.name,
+				});
+				infowindow.open(map, marker);
+			});
+		});
+
+	}).catch(err => {
+		console.log("OOPS!", err);
+	})	
+}
+
+
+//add API key
+src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=&v=weekly"
+async
